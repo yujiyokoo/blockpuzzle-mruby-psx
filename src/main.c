3ebb465270351fd7af8b470cb5e06ebab6696776
Yuji Yokoo <yujiyokoo@gmail.com>
2025-02-18 00:08:57 +0900

    Initial compilable (not playable) state

---
+++ src/main.c
@@ -0,0 +1,274 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <psxgpu.h>
+#include <mruby.h>
+#include <mruby/irep.h>
+#include <mruby/string.h>
+#include <mruby/array.h>
+
+static uint32_t *xfb = NULL;
+// static GXRModeObj *rmode = NULL;
+
+#define BUFSIZE 100
+
+struct InputBuf {
+  uint16_t buffer[BUFSIZE];
+  uint32_t index;
+} input_buf;
+
+static mrb_value btn_mrb_buffer;
+
+extern const uint8_t program[];
+
+static mrb_value print_msg(mrb_state *mrb, mrb_value self) {
+  char *unwrapped_content;
+  mrb_value str_content;
+
+  mrb_get_args(mrb, "S", &str_content);
+  unwrapped_content = mrb_str_to_cstr(mrb, str_content);
+  printf("%s\n", unwrapped_content);
+
+  return mrb_nil_value();
+}
+
+// TODO: Just to make compiler happy
+uint32_t PAD_BUTTON_START = 1;
+uint32_t PAD_BUTTON_LEFT  = 2;
+uint32_t PAD_BUTTON_RIGHT = 4;
+uint32_t PAD_BUTTON_UP    = 8;
+uint32_t PAD_BUTTON_DOWN  = 16;
+uint32_t PAD_BUTTON_A     = 32;
+uint32_t PAD_BUTTON_B     = 64;
+
+static mrb_value get_button_masks(mrb_state *mrb, mrb_value self) {
+  mrb_value mask_array;
+  mask_array = mrb_ary_new(mrb);
+
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_START));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_LEFT));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_RIGHT));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_UP));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_DOWN));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_A));
+  mrb_ary_push(mrb, mask_array, mrb_fixnum_value(PAD_BUTTON_B));
+
+  return mask_array;
+}
+
+static uint32_t CvtRGB (uint8_t r1, uint8_t g1, uint8_t b1, uint8_t r2, uint8_t g2, uint8_t b2)
+{
+	int y1, cb1, cr1, y2, cb2, cr2, cb, cr;
+
+	y1 = (299 * r1 + 587 * g1 + 114 * b1) / 1000;
+	cb1 = (-16874 * r1 - 33126 * g1 + 50000 * b1 + 12800000) / 100000;
+	cr1 = (50000 * r1 - 41869 * g1 - 8131 * b1 + 12800000) / 100000;
+
+	y2 = (299 * r2 + 587 * g2 + 114 * b2) / 1000;
+	cb2 = (-16874 * r2 - 33126 * g2 + 50000 * b2 + 12800000) / 100000;
+	cr2 = (50000 * r2 - 41869 * g2 - 8131 * b2 + 12800000) / 100000;
+
+	cb = (cb1 + cb2) >> 1;
+	cr = (cr1 + cr2) >> 1;
+
+	return (y1 << 24) | (cb << 16) | (y2 << 8) | cr;
+}
+
+static uint32_t PACK_PIXEL(int r, int g, int b) {
+  return CvtRGB(r, g, b, r, g, b);
+}
+
+static mrb_value draw20x20_640(mrb_state *mrb, mrb_value self) {
+  mrb_int x, y, r, g, b;
+  mrb_get_args(mrb, "iiiii", &x, &y, &r, &g, &b);
+
+  int i = 0, j = 0;
+
+  if(r == 0 && g == 0 && b == 0) {
+    for(i = 0; i < 20; i++) {
+      for(j = 0; j < 10; j++) {
+        xfb[x+j + (y+i) * 320] = PACK_PIXEL(r, g, b);
+      }
+    }
+  } else {
+    int r_light = (r+128 <= 255) ? r+128 : 255;
+    int g_light = (g+128 <= 255) ? g+128 : 255;
+    int b_light = (b+128 <= 255) ? b+128 : 255;
+
+    int r_dark = (r-64 >= 0) ? r-64 : 0;
+    int g_dark = (g-64 >= 0) ? g-64 : 0;
+    int b_dark = (b-64 >= 0) ? b-64 : 0;
+
+    // TODO: implement lines and use them.
+    for(j = 0; j < 10; j++) {
+      xfb[x+j + (y) * 320] = PACK_PIXEL(30, 30, 30);
+      xfb[x+j + (y+19) * 320] = PACK_PIXEL(30, 30, 30);
+    }
+    for(j = 1; j < 9; j++) {
+      xfb[x+j + (y+1) * 320] = PACK_PIXEL(r_light, g_light, b_light);
+    }
+    for(j = 2; j < 10; j++) {
+      xfb[x+j + (y+19) * 320] = PACK_PIXEL(r_dark, g_dark, b_dark);
+    }
+    for(i = 2; i < 19; i++) {
+      xfb[x + (y+i) * 320] = PACK_PIXEL(30, 30, 30);
+      xfb[x+1 + (y+i) * 320] = PACK_PIXEL(r_light, g_light, b_light);
+      for(j = 2; j < 9; j++) {
+        xfb[x+j + (y+i) * 320] = PACK_PIXEL(r, g, b);
+      }
+      xfb[x+9 + (y+i) * 320] = PACK_PIXEL(r_dark, g_dark, b_dark);
+    }
+  }
+
+  return mrb_nil_value();
+}
+
+static mrb_value init_controller_buffer(mrb_state *mrb, mrb_value self) {
+  btn_mrb_buffer = mrb_ary_new(mrb);;
+  input_buf.index = 0;
+
+  int i = 0;
+  while(i < BUFSIZE) {
+    mrb_ary_set(mrb, btn_mrb_buffer, i, mrb_nil_value());
+    input_buf.buffer[i] = 0; i ++ ;
+  }
+
+  return mrb_nil_value();
+}
+
+void *read_buttons() {
+/*
+  while(1) {
+    input_buf.index = (input_buf.index + 1) % BUFSIZE;
+		PAD_ScanPads();
+		uint16_t btns = PAD_ButtonsHeld(0);
+		input_buf.buffer[input_buf.index] = btns;
+
+		LWP_YieldThread();
+  }
+*/
+}
+
+static mrb_value start_controller_reader(mrb_state *mrb, mrb_value self) {
+  // lwp_t thread;
+  // LWP_CreateThread(&thread, read_buttons, NULL, NULL, 0, 0);
+  return mrb_fixnum_value(0);
+}
+
+static mrb_value get_button_state(mrb_state *mrb, mrb_value self) {
+  // PAD_ScanPads();
+	// uint16_t btns = PAD_ButtonsHeld(0);
+  // return mrb_fixnum_value(btns);
+  return mrb_fixnum_value(0);
+}
+
+static mrb_value get_button_states(mrb_state *mrb, mrb_value self) {
+  // unimplemented
+  return mrb_fixnum_value(0);
+}
+
+static mrb_value start_btn(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_START);
+}
+
+static mrb_value dpad_left(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_LEFT);
+}
+
+static mrb_value dpad_right(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_RIGHT);
+}
+
+static mrb_value dpad_up(mrb_state *mrb, mrb_value self) {
+  // unimplemented
+  return mrb_fixnum_value(0);
+}
+
+static mrb_value dpad_down(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_DOWN);
+}
+
+static mrb_value btn_a(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_A);
+}
+
+static mrb_value btn_b(mrb_state *mrb, mrb_value self) {
+  mrb_int state;
+  mrb_get_args(mrb, "i", &state);
+
+  return mrb_bool_value(state & PAD_BUTTON_B);
+}
+
+static mrb_value clear_score(mrb_state *mrb, mrb_value self) {
+  char* clear_str = "Press START";
+  printf("\x1b[8;53H");
+  printf("%s", clear_str);
+  return mrb_nil_value();
+}
+
+static mrb_value render_score(mrb_state *mrb, mrb_value self) {
+  mrb_int score;
+  mrb_get_args(mrb, "i", &score);
+  char buf[20];
+  snprintf(buf, 20, "Score: %8" PRId32, score);
+  printf("\x1b[8;53H");
+  printf("%s", buf);
+
+  return mrb_nil_value();
+}
+
+static mrb_value get_current_button_index(mrb_state *mrb, mrb_value self) {
+  // unimplemented
+  return mrb_fixnum_value(0);
+}
+
+static mrb_value waitvbl(mrb_state *mrb, mrb_value self) {
+  // VIDEO_WaitVSync();
+  return mrb_nil_value();
+}
+
+static mrb_value get_next_button_state(mrb_state *mrb, mrb_value self) {
+  mrb_int wanted_index;
+  mrb_get_args(mrb, "i", &wanted_index);
+  int curr_index = input_buf.index;
+
+  if(wanted_index >= BUFSIZE || wanted_index < 0) { wanted_index = wanted_index % BUFSIZE; }
+
+  if(wanted_index == (curr_index + 1) % BUFSIZE) {
+    return mrb_nil_value();
+  } else {
+    return mrb_fixnum_value(input_buf.buffer[wanted_index]);
+  }
+}
+
+int errno = 0;
+
+int main(int argc, char **argv) {
+  ResetGraph(0);
+  FntLoad(960, 0);
+
+  mrb_state *mrb = mrb_open();
+  if (!mrb) { return 1; }
+  struct RClass *psx_mruby = mrb_define_module(mrb, "PsxMruby");
+  mrb_define_module_function(mrb, psx_mruby, "print_msg", print_msg, MRB_ARGS_REQ(1));
+
+  printf("**************************************\n\n");
+  mrb_load_irep(mrb, program);
+  printf("**************************************\n\n");
+
+	return 0;
+}
---
+++ src/program.rb
@@ -0,0 +1,267 @@
+BOARD = Array.new(20) { Array.new(10, ' ') }
+def add_to_board(block, x, y)
+  rows = block.rows
+  rows.each_with_index { |row, idx|
+    row.split('').each_with_index { |cell, i|
+      BOARD[y + idx + block.y][x + i + block.x] = if cell == ' '
+        BOARD[y + idx + block.y][x + i + block.x]
+      else
+        cell
+      end
+    }
+  }
+end
+
+def can_drop?(block, x, y)
+  return can_move_to?(block, x, y+1)
+end
+
+def can_move_to?(block, x, y)
+  return false if x < 0 - block.x or x + block.x + block.width > 10
+  return false if y + block.y + block.height > HEIGHT
+  block_rows = block.rows.map { |r| r.split('') }
+
+  block_rows.each_with_index { |block_row, idx|
+    board_row = BOARD[y+block.y+idx].slice(x+block.x, block_row.length)
+    board_row.zip(block_row).each { |zipped_elem|
+      return false if zipped_elem[0] != ' ' && zipped_elem[1] != ' '
+    }
+  }
+
+  return true
+end
+
+def square(cell)
+  PsxMruby.print_msg("cell is: #{cell.inspect}")
+  if cell == ' '
+    '  '
+  else
+    '[]'
+  end
+end
+
+def rotate_r(block, x, y)
+  # $log.puts("block is: #{block.inspect}")
+  block.spin_forward
+  block.spin_backwards unless can_move_to?(block, x, y)
+
+  return block, x, y
+end
+
+def delete_full_rows
+  to_delete = []
+  BOARD.each_with_index { |row, idx|
+    if row.all? { |cell| cell != ' ' }
+      to_delete.push idx
+    end
+  }
+
+  $score += to_delete.size * to_delete.size
+
+  to_delete.each { |idx|
+    BOARD.delete_at(idx)
+    BOARD.unshift(Array.new(10, ' '))
+  }
+end
+
+class Block
+  MAPPING = {
+    I: [["6666", 0, 1], ["6\n6\n6\n6", 2, 0], ["6666", 0, 2], ["6\n6\n6\n6", 1, 0]],
+    J: [["4\n444", 1, 1], ["44\n4\n4", 2, 0], ["444\n  4", 1, 1], [" 4\n 4\n44", 2, 0]],
+    L: [["777\n7", 1, 1], ["77\n 7\n 7", 2, 0], ["  7\n777", 1, 1], ["7\n7\n77", 2, 0]],
+    O: [["33\n33", 1, 1]],
+    T: [["555\n 5", 1, 1], ["5\n55\n5", 2, 0], [" 5 \n555", 1, 1], [" 5\n55\n 5", 2, 0]],
+    S: [[" 22\n22", 1, 1], ["2\n22\n 2", 2, 0]],
+    Z: [["11\n 11", 1, 1], [" 1\n11\n1", 2, 0]]
+  }
+
+  def initialize(shape, rotation = 0)
+    @shape = shape
+    @rotation = rotation
+  end
+
+  def x
+    MAPPING[@shape][@rotation][1]
+  end
+
+  def y
+    MAPPING[@shape][@rotation][2]
+  end
+
+  def spin_forward
+    @rotation = (@rotation + 1) % MAPPING[@shape].size
+  end
+
+  def spin_backwards
+    @rotation = (@rotation - 1) % MAPPING[@shape].size
+  end
+
+  def rows
+    spin_to_current[0].split("\n")
+  end
+
+  def spin_to_current
+    positions = MAPPING[@shape]
+    positions[@rotation % positions.size]
+  end
+
+  def width
+    rows.map(&:length).max
+  end
+
+  def height
+    rows.size
+  end
+end
+
+def render_squares(win, row)
+  row.split('').each { |ch|
+    # TODO: colours
+    win << square(ch)
+  }
+end
+
+def render_next_block(win, block)
+  x = 15
+  y = 2
+
+  (0..3).each { |i|
+    win.setpos(y + i, x * 2)
+    win << "                "
+  }
+
+  block.rows.each_with_index { |row, i|
+    win.setpos(y + i + block.y, (x + 1 + block.x) * 2)
+    render_squares(win, row)
+  }
+end
+
+class FakeCurses
+  def initialize
+    @x = @y = 0
+  end
+
+  def setpos(y, x)
+    @x = x
+    @y = y
+  end
+
+  def <<(str)
+    PsxMruby.print_msg(str)
+  end
+end
+
+$score = 0
+
+class MainGame
+  HEIGHT = 20
+
+  def initialize
+    # TODO: randomness
+    @curr_random = 0
+  end
+
+  def random_block
+    # TODO: randomness
+    blocks = [:I, :J, :L, :O, :S, :Z,:T]
+    #blocks[rand(blocks.size)]
+    @curr_random = (@curr_random + 1) % blocks.size
+    blocks[@curr_random]
+  end
+
+  def main_loop
+    win = FakeCurses.new
+    x = 4
+    y = 0
+    current_frame = 0
+    wait_frames = 20
+    block_count = 0
+    blocks_per_level = 15
+    start_frame = 0
+    curr_block = Block.new(random_block, 0)
+    next_block = Block.new(random_block, 0)
+    render_next_block(win, next_block)
+    loop do
+      cb_rows = curr_block.rows
+      (0..HEIGHT-1).each { |h|
+        win.setpos(h, 0)
+        win << "##"
+        render_squares(win, BOARD[h].join)
+        win.setpos(h, 11*2)
+        win << "##"
+        cb_rows.each_with_index { |cbr, i|
+          offset = (cbr.size - cbr.strip.size)
+          win.setpos(y+i + curr_block.y, (x + 1 + curr_block.x + offset) * 2)
+          render_squares(win, cbr.strip)
+        }
+        # TODO: flip here win.refresh
+      }
+      win.setpos(HEIGHT, 0)
+        win << ("##" * 12)
+
+      win.setpos(HEIGHT+3, 4)
+        win << ("Score #{$score * 100}")
+
+      # TODO:input
+      # str = STDIN.read_nonblock(1, exception: false)
+      str = 'a'
+      case str
+      when 'h'
+        x = x-1 if can_move_to?(curr_block, x-1, y)
+      when 'l'
+        x = x+1 if can_move_to?(curr_block, x+1, y)
+      when 'j'
+        y = y+1 if can_move_to?(curr_block, x, y+1)
+      when ' '
+        curr_block, x, y = rotate_r(curr_block, x, y)
+      end
+
+
+      elapsed = current_frame - start_frame
+      if elapsed > wait_frames
+        if can_drop?(curr_block, x, y)
+          y += 1
+        else
+          # $log.puts("curr_block, x, y: #{curr_block}, #{x}, #{y}")
+          add_to_board(curr_block, x, y)
+          block_count += 100
+          wait_frames = [wait_frames - 3, 5].max if block_count % blocks_per_level == 0
+          delete_full_rows
+          y = 0
+          x = 4
+          curr_block = next_block
+          next_block = Block.new(random_block, 0)
+          render_next_block(win, next_block)
+          unless can_move_to?(curr_block, x, y)
+            win.setpos(8, 0)
+            win << "                        "
+            win.setpos(9, 0)
+            win << "       GAME OVER        "
+            win.setpos(10, 0)
+            win << "                        "
+            win.setpos(11, 0)
+            win << "   PRESS ENTER TO EXIT  "
+            win.setpos(12, 0)
+            win << "                        "
+            loop {
+              c = win.getch
+              break if c == 10 || c == 13
+            }
+            break
+          end
+        end
+        start_frame = current_frame
+      end
+    end
+  end
+end
+
+begin
+  PsxMruby.print_msg 'Starting the game.'
+  MainGame.new.main_loop
+rescue => ex
+  # Note backtrace is only available when you pass -g to mrbc
+  PsxMruby.print_msg ex.inspect
+  PsxMruby.print_msg ex.backtrace.inspect
+  raise ex
+end
